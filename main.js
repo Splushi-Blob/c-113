fee703fabfb6b86547b61090e5d7cdfbb7c561ce
Splushi-Blob <115597352+Splushi-Blob@users.noreply.github.com>
2023-08-29 20:59:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300)
+    canvas.center()
+}
+
+function modelLoaded(){
+    console.log("poseNet is loaded")
+}
+
+function draw(){
+  
+}
+ function take_snapshot(){
+    
+ }
